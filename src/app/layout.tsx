7269b80a67783356001382a523720d7c772ee648
Het Gajera <het36100@gmail.com>
2025-01-21 00:14:36 +0530

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+import { QueryProvider } from "@/components/query-provider";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +31,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <QueryProvider>{children}</QueryProvider>
       </body>
     </html>
   );
